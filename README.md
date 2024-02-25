66aae205a0706633a38a9827a93165bd0685a25d
Jared588 <jaredeprice@gmail.com>
2024-02-25 05:55:44 +0200

    Add basic template/styling to Homepage

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # shopping-cart
 Commercial shopping-application
+
+Photo by <a href="https://unsplash.com/@snapsbyclark?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Clark Van Der Beken</a> on <a href="https://unsplash.com/photos/a-black-and-white-photo-of-a-metal-wall-BSjlgIx4gBQ?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+  
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,18 @@
 function Home() {
   return (
-    <div className="flex border-b justify-between">
-      <h1 className="py-10 px-10 text-6xl">Cool Corner</h1>
-      <nav className="flex items-center text-2xl px-10 space-x-10 *:cursor-pointer">
-        <p>Home</p>
-        <p>Store</p>
-        <p>Contact Us</p>
-      </nav>
+    <div className="flex flex-col min-h-svh justify-between bg-black bg-opacity-50">
+      <header className="flex flex-col text-center justify-between sm:flex-col md:flex-row lg:flex-row">
+        <h1 className="py-7 px-10 text-6xl">Cool Corner</h1>
+        <nav className="flex items-center justify-center text-2xl px-10 space-x-10 *:cursor-pointer">
+          <p>Home</p>
+          <p>Store</p>
+          <p>Contact Us</p>
+        </nav>
+      </header>
+      <div className="text-5xl bg-black bg-opacity-50 border-y flex flex-auto justify-center items-center text-center">
+        Welcome to the COOOOOOOL Corner, <br />check out our awesome catalogue
+      </div>
+      <footer className="text-center">Copyright © Jared588</footer>
     </div>
   );
 }
