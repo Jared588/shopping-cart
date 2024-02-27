142e1126a7600dd8984bd6cce2121acc4830fea5
Jared588 <jaredeprice@gmail.com>
2024-02-27 06:37:49 +0200

    Improve errorPage

--- src/routes/ErrorPage.jsx
+++ src/routes/ErrorPage.jsx
@@ -2,9 +2,9 @@ import { Link } from 'react-router-dom';
 
 const ErrorPage = () => {
   return (
-    <div className='bg-black bg-opacity-85 flex min-h-screen'>
+    <div className='bg-black bg-opacity-85 flex flex-col min-h-screen items-center justify-center text-4xl'>
       <h1>Oh no, this route doesn&apos;t exist!</h1>
-      <Link to="/">
+      <Link to="/" className='underline'>
         You can go back to the home page by clicking here, though!
       </Link>
     </div>
