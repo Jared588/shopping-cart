92ea6c4c2c1139dbaa28e6db09e7f14f4ba9fc37
Jared588 <jaredeprice@gmail.com>
2024-02-28 10:07:56 +0200

    Update tests

--- tests/App.test.jsx
+++ tests/App.test.jsx
@@ -1,10 +1,10 @@
 import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
-import App from '../src/App.jsx';
+import Router from '../src/routes/Router.jsx';
 
 describe('App component', () => {
   it('renders the home component', () => {
-    render(<App />);
-    expect(screen.getByText(/Cool Corner/i)).toBeInTheDocument();
+    render(<Router />);
+    expect(screen.getByText(/Style. Simplified/i)).toBeInTheDocument();
   });
 });
--- tests/Home.test.jsx
+++ tests/Home.test.jsx
@@ -1,19 +1,19 @@
 import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
-import Home from '../src/Home';
+import Router from '../src/routes/Router';
 
 describe('Home component', () => {
     it('renders correct heading', () => {
-        render(<Home />);
-        expect(screen.getByRole('heading').textContent).toMatch(/Cool Corner/i);
+        render(<Router />);
+        expect(screen.getByRole('heading').textContent).toMatch(/S/i);
     })
 
     it('renders navbar', () => {
-        render(<Home />);
-        const navElement = screen.getByRole('navigation');
+        render(<Router />);
+        const navElement = screen.getByRole('navigation', {name: /primary-nav/});
         expect(navElement).toBeInTheDocument(); 
-        expect(navElement.children[0].textContent).toMatch(/Home/i);
-        expect(navElement.children[1].textContent).toMatch(/Store/i);
-        expect(navElement.children[2].textContent).toMatch(/Contact Us/i);
+        expect(navElement.children[0].textContent).toMatch(/Men/i);
+        expect(navElement.children[1].textContent).toMatch(/Women/i);
+        expect(navElement.children[2].textContent).toMatch(/Sale/i);
     })
 })
