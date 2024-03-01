1ecbc4a2c34897bdef7e553fd2023a8ae323df8d
Jared588 <jaredeprice@gmail.com>
2024-03-01 09:46:23 +0200

    Add shoes to catalogue

--- src/routes/Store.jsx
+++ src/routes/Store.jsx
@@ -39,6 +39,11 @@ function Store() {
                             {shirt}
                         </div>
                     ))}
+                    {shoes.map((shoe, index) => (
+                        <div key={index}>
+                            {shoe}
+                        </div>
+                    ))}
                   </div>
                 </div>
             </div>
@@ -47,14 +52,30 @@ function Store() {
 }
 
 const shirts = [
+    // C-neck
     CreateItem('shirtC', 'T-shirt - white', '$39.99', 'white'),
     CreateItem('shirtC', 'T-shirt - grey', '$39.99', 'gray'),
     CreateItem('shirtC', 'T-shirt - black', '$39.99', 'black'),
     CreateItem('shirtC', 'T-shirt - silver', '$39.99', 'silver'),
-
+    // V-neck
     CreateItem('shirtV', 'T-shirt - purple', '$39.99', 'purple'),
     CreateItem('shirtV', 'T-shirt - pink', '$39.99', 'pink'),
     CreateItem('shirtV', 'T-shirt - teal', '$39.99', 'teal'),
 ]
 
+const shoes = [
+    // Formal
+    CreateItem('shoeFormal', 'Shoe - white', '$39.99', 'white'),
+    CreateItem('shoeFormal', 'Shoe - black', '$39.99', 'black'),
+    CreateItem('shoeFormal', 'Shoe - gray', '$39.99', 'gray'),
+    // Heel
+    CreateItem('shoeHeel', 'Heel - black', '$39.99', 'black'),
+    CreateItem('shoeHeel', 'Heel - red', '$39.99', 'red'),
+    CreateItem('shoeHeel', 'Heel - blue', '$39.99', 'blue'),
+    // Sneaker
+    CreateItem('shoeSneaker', 'Sneaker - white', '$39.99', 'white'),
+    CreateItem('shoeSneaker', 'Sneaker - black', '$39.99', 'black'),
+    CreateItem('shoeSneaker', 'Sneaker - gray', '$39.99', 'gray'),
+
+]
 export default Store
