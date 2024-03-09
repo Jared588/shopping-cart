7a46a838a5d02a6e8f83448323634dca2a38f06a
Jared588 <jaredeprice@gmail.com>
2024-03-09 19:26:34 +0200

    Improve styling in Store

--- src/routes/Store.jsx
+++ src/routes/Store.jsx
@@ -12,7 +12,7 @@ function Store({ showItemAddedMessage }) {
             <Header />
             <div className="text-5xl bg-black bg-opacity-80 border-t flex flex-col lg:flex-row flex-auto md:px-16 lg:py-10 overflow-hidden">
                 <nav className='flex-col lg:pr-8 mb-10 border-b lg:border-r lg:border-b-0 lg:flex'>
-                    <h2 aria-label='filter-heading' className='hidden lg:block pb-6 text-4xl font-semibold slide-in text-slate-500 text-center'>Categories</h2>
+                    <h2 aria-label='filter-heading' className='hidden lg:block pb-2 text-4xl font-semibold slide-in text-slate-500 text-center'>Categories</h2>
                     <ul aria-label='categories' className="flex justify-between py-5 lg:flex-col text-center lg:text-left text-2xl px-2 pb-4 lg:space-y-4 *:cursor-pointer slide-in-2">
                         <li onClick={() => setFilter('shirts')}>Shirts</li>
                         <li onClick={() => setFilter('shoes')}>Shoes</li>
